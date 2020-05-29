e8dbc3c3b1761f7a92b4f0ac0f2bd78b43eac3e4
Hitaylor <sportsina@126.com>
2020-05-29 11:08:21 +0800

    master

--- kernel/file.c
+++ kernel/file.c
@@ -2,11 +2,8 @@
 #include <uapi/linux/uio.h>
 #include <linux/uio.h>
 #include <linux/slab.h>
-<<<<<<< HEAD
 #include <asm/uaccess.h>
 //#include "itree.h"
-=======
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
 
 static int episode_setattr(struct dentry *dentry, struct iattr *attr)
 {
@@ -74,7 +71,6 @@ static ssize_t episode_direct_write(struct file *filp, const char __user *buf, s
          * Prev next   timestamp        offset  len   data
          * 8    8       4               8       4       k       
          * ****************************************************************/
-<<<<<<< HEAD
         char * buff = NULL, *buff2=NULL;
        
         __u32 pos = 0;//buf内的偏移量
@@ -92,23 +88,6 @@ static ssize_t episode_direct_write(struct file *filp, const char __user *buf, s
         __u64 curPos;
         __u64 lastRecPos = 0;
         int retnum = 0;
-=======
-        char * buff = NULL;
-       
-        __u32 pos = 0;//buf内的偏移量
-        __u32  dataSizeinRec = 0;//buf内当前数据记录的数据段长度
-        __u32 tmp= 0;
-        __u32 bufLen = 0;//buf的长度
-        __u32 timestamp = 0;//时间戳
-        __u32 position=0;
-        __u64 prev = 0,next = 0,offset = 0;//在文件中，当前记录的前一个记录的起始位置、next字段起始位置，以及当前记录的数据段位置
-        char  lenSeg[sizeof(dataSizeinRec)],time[sizeof(timestamp)];//用于记录长度和时间的临时变量
-        char * ptr8;
-        int i;
-        __u64 curPos;
-        __u64 lastRecPos = 0;
-        
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
 	//读取inode中的上一条记录的位置，jsc 0510
         
         struct episode_inode * raw_inode;
@@ -128,20 +107,14 @@ struct iovec iov;
         }
         printk("I am here! and the user buf size is %d\n" ,len);
         lastRecPos = episode_inode->i_lastrecordpos;
-<<<<<<< HEAD
         tmpLen = i_lastrecordpos(inode);
         curPos = inode->i_size;//文件游标位置,也是这次写操作的base
         printk("The last data record position: %ld\t tmpLen=%lld\n current postion:%ld\n",lastRecPos,tmpLen,curPos);
-=======
-        curPos = inode->i_size;//文件游标位置
-        printk("The last data record position: %ld\n current postion:%ld\n",lastRecPos,curPos);
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
         if(len%512 != 0)
         {
            printk("user buf len %lu mod 512!=0, now return -1! \n",len);
            return -1;
         }
-<<<<<<< HEAD
         printk("len=%d\n",len);
         //这里，后续可以先读取一遍buf，得到具体的record数量n，因为对于每条record，扩展需要添加的字节数是固定的，8+8+8+4=28字节，则比原来需要增加28n字节，然后将len+28n向上取512的整数倍，即为buff的长度
        // buff = memalign(512,(1+len/512)*512);
@@ -151,17 +124,11 @@ struct iovec iov;
         memset(buff,0,(1+len/512)*512);
         // printk("ater memset, buff:");
         //for(i=0;i<1024;i++) printk("buff[%d]=%u",i,buff[i]<0?(255+buff[i]):buff[i]);
-=======
-        //这里，后续可以先读取一遍buf，得到具体的record数量n，因为对于每条record，扩展需要添加的字节数是固定的，8+8+8+4=28字节，则比原来需要增加28n字节，然后将len+28n向上取512的整数倍，即为buff的长度
-       // buff = memalign(512,(1+len/512)*512);
-        buff = kmalloc((1+len/512)*sizeof(char), GFP_KERNEL);
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
         if(!buff)
         {
            printk("kmalloc failed for the buff!\n");
         }
         printk("buff size %d\n",sizeof(buff));
-<<<<<<< HEAD
         printk("I am here 2!\n");
         buff2 = (char *)kmalloc((1+len/512)*512, GFP_KERNEL);
         memset(buff2,0,(1+len/512)*512);
@@ -209,42 +176,6 @@ struct iovec iov;
            position = position +sizeof(curPos);
            
            
-=======
-
-        while(pos < bufLen-1){ //遍历buf中的每一条记录，进行扩充，形成新的结构，然后放到buff中。
-           mid(lenSeg, buf, pos, pos+sizeof(dataSizeinRec));//获取buf中一条记录的长度字段
-           //dataSizeinRec = atoi(lenSeg);//不能使用atoi转换成数字长度，因为它是将“12345”转成12345，前面的"12345“占5位，而int实际上只占4位，在buf中应该是12345的4字节的二进制表示
-           if(!lenSeg){
-               printk("lenSeg parse failed!\n"); 
-           }
-           memcpy(&dataSizeinRec,lenSeg,sizeof(dataSizeinRec));//将lenSeg表达的内容强制转换成int
-           printk("data size in the record is %d\n",dataSizeinRec);
-           //构造索引结构和索引信息
-           //prev,next,timestamp,offset,len,data
-           prev = lastRecPos;
-           next = curPos+sizeof(prev)+sizeof(next)+sizeof(timestamp)+sizeof(offset)+sizeof(dataSizeinRec)+dataSizeinRec+sizeof(prev);
-           timestamp = getCurrentTime();
-           offset = curPos;
-           len = dataSizeinRec;
-           //先用itoa，再使用memcpy？
-           //itoa(prev,ptr8,10);
-           memcpy(&buff[position],&prev,sizeof(prev));
-           position = position +sizeof(prev);
-           //ptr8 = NULL;
-           
-           //itoa(next,ptr8,10);
-           memcpy(&buff[position],&next,sizeof(next));
-           position = position +sizeof(next);
-           //ptr8 = NULL;
-           
-           //itoa(timestamp,time,10);
-           memcpy(&buff[position],&timestamp,sizeof(timestamp));
-           position = position +sizeof(timestamp);
-           
-           //itoa(curPos,ptr8,10);
-           memcpy(&buff[position],&curPos,sizeof(curPos));
-           position = position +sizeof(curPos);
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
            //ptr8 = NULL;
            /*
            memcpy(&buff[position],lenSeg,sizeof(dataSizeinRec));
@@ -253,7 +184,6 @@ struct iovec iov;
            position = position +len-4;
            */
           //copy buf中该数据记录到buff中,替换上面的两次memcpy()调用
-<<<<<<< HEAD
           memcpy(&buff2[position],&buff[pos],sizeof(recLen)+recLen);
           printk("recLen segment:");
            for(i=0;i<sizeof(recLen);i++) printk("buff2[%d]=%ud",position+i,buff2[position+i]<0 ? (255+buff2[position+i]):buff2[position+i]);
@@ -307,25 +237,6 @@ struct iovec iov;
         iov.iov_base = (void __user *)buf;
         //iov.iov_len = position+1;//此时buff的长度应为512整数倍。但这里的position+1却不一定，所以这里填写什么需要确认一下
 	iov.iov_len = len;
-=======
-          memcpy(&buff[position],&buf[pos],sizeof(dataSizeinRec)+dataSizeinRec);
-
-           pos = pos+sizeof(dataSizeinRec)+dataSizeinRec;
-           printk("current pos:%d\n",pos);
-        }
-        printk("After extending, data in buff are:\n");
-        for(i=0;i<pos;i++){
-                printk("%c",buff[i]);
-        }
-        printk("\n");
-        
-
-        //修改buf，jsc
-       // struct iovec iov = { .iov_base = (void __user *)buf, .iov_len = len };
-        iov.iov_base = (void __user *)buff;
-        iov.iov_len = position+1;//此时buff的长度应为512整数倍。但这里的position+1却不一定，所以这里填写什么需要确认一下
-	
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
         
 
 	init_kiocb(&kiocb, filp);
@@ -342,10 +253,7 @@ struct iovec iov;
         }
         
         kfree(buff);
-<<<<<<< HEAD
         kfree(buff2);
-=======
->>>>>>> 63c40a9d1851e5c1ebf3a48d52da4f195fedccf9
         return ret;
 }
 
